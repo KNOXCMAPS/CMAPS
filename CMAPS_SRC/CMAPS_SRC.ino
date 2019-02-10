d70b54ff5ee2fbc1d3174de3685b3cbbdb9352b6
KalJay <kalj121@gmail.com>
2019-02-10 20:42:41 +1100

    Reduced memory usage overall to permit sending to web server

--- CMAPS_SRC/CMAPS_SRC.ino
+++ CMAPS_SRC/CMAPS_SRC.ino
@@ -1,7 +1,5 @@
-#include <Adafruit_FONA.h>
-
 #include "Adafruit_FONA.h"
-#include <stdlib.h>
+#include "Adafruit_FONA_3G.h"
 
 #define FONA_TX 2
 #define FONA_RX 3
@@ -15,31 +13,20 @@ SoftwareSerial *fonaSerial = &fonaSS;
 HardwareSerial *fonaSerial = &Serial1;
 #endif
 
+////////////  OLED I2C DISPLAY /////////////////////////
 #include <Arduino.h>
-#include <U8g2lib.h>
+#include <U8x8lib.h>
 
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
 #endif
-#ifdef U8X8_HAVE_HW_I2C
-#include <Wire.h>
-#endif
 
-U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); //Using the page buffer part of this library, to be less RAM intensive (full buffer + FONA was exceeding the memory on the Arduino)
-                                                                             //Should RAM usage be a problem in the future, consider using the Uxgx part instead (loses some other graphical features, but even further reduced memory usage)
+// IMPORTANT "DeviceUUID" defines the unique ID. LABEL THE ARDUINO UNIT with the number after upload.
+const int DeviceUUID = 102;
 
-bool devMode = true; //Turns on debug messages to the OLED Display, consider keeping on all the time for feedback considerations?
-
-using namespace std;
-
-const int DeviceUUID = 502; // IMPORTANT "DeviceUUID" defines the unique ID. LABEL THE ARDUINO UNIT with the number after upload.
-
-// Use this for FONA 800 and 808s
-//Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
-// Use this one for FONA 3G
-Adafruit_FONA_3G fona = Adafruit_FONA_3G(FONA_RST);    
-//For Auto Restart    
-int RestartCount;   
+Adafruit_FONA_3G fona = Adafruit_FONA_3G(FONA_RST); 
+U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);   
+  
 // floatToString.h    
 //    
 // Tim Hirzel   
@@ -47,24 +34,6 @@ int RestartCount;
 // March 2008   
 // float to string    
 //    
-// If you don't save this as a .h, you will want to remove the default arguments    
-//     uncomment this first line, and swap it for the next.  I don't think keyword arguments compile in .pde files    
-
-void OLEDPrint(String text) { //Use for printing one line of text on the OLED display. Does not consider line length at all and will just print what fits within one line
-    const char* string1 = text.c_str(); //We must use character pointers since u8g2.drawStr() requires a character pointer in its parameters (not a string!), note that it will treat this pointer like a character array and iterate until it finds the null
-    u8g2.clear();
-      do {
-        u8g2.drawStr(0,12,string1);
-      } while ( u8g2.nextPage() );
-  }
-
-void debugPrint(String text) { //Call this when printing something of debug/devMode purpose to the OLED
-      if(devMode) {
-          OLEDPrint(text);  
-      }
-  }
-  
-
 char * floatToString(char * outstr, float value, int places, int minwidth, bool rightjustify=false) {   
     // this is used to write a float value to string, outstr.  oustr is also the return value.    
     int digit;    
@@ -131,11 +100,9 @@ char * floatToString(char * outstr, float value, int places, int minwidth, bool 
     }
 
     // if no places after decimal, stop now and return
-
     // otherwise, write the point and continue on
     if (places > 0)
     outstr[c++] = '.';
-
 
     // now write out each decimal place by shifting digits one by one into the ones place and writing the truncated value
     for (i = 0; i < places; i++) {
@@ -150,150 +117,86 @@ char * floatToString(char * outstr, float value, int places, int minwidth, bool 
             outstr[c++] = ' ';
         }
     }
+    
     outstr[c++] = '\0';
-    return outstr;  
+    return outstr;
 }
 
-void setup() {
-  u8g2.begin();
-  u8g2.clear();
-  u8g2.setFont(u8g2_font_6x10_tr); //Many different fonts avaliable at https://github.com/olikraus/u8g2/wiki/fntlistall
-  while (!Serial);
-    
-   // Auto restart
 
-   RestartCount = 0;
-   
-  Serial.begin(115200);
-  Serial.println(F("CMAPS GPS sketch loaded"));
-  Serial.println(F("Initializing....(May take 3 seconds)"));
-  debugPrint("Starting...");
+
+void setup() {
+  while (!Serial);
+
+  u8x8.begin();
+  u8x8.setFont(u8x8_font_amstrad_cpc_extended_f);
+  
+  u8x8.clear();
+  u8x8.println("Starting...");
 
   fonaSerial->begin(4800);
   if (! fona.begin(*fonaSerial)) {
-    Serial.println(F("Couldn't find FONA"));
-    debugPrint("No FONA Found");
+    u8x8.clear();
+    u8x8.println("No FONA");
     while(1);
   }
-  Serial.println(F("FONA is OK"));
-  debugPrint("FONA is OK");
   
   while (true) {
     uint8_t stat = fona.getNetworkStatus();
     if ((stat == 1) || (stat == 5)){
-      Serial.print(F("Network associated!: "));
-      Serial.println(stat);
-      debugPrint("Network Associated");
       break;
     }
-    delay(500);
-      Serial.print(F("waiting for network: "));
-      Serial.println(stat);
-      debugPrint("Waiting for network");
   }
-  
-  Serial.println(F("Setting APN"));
-  debugPrint("Setting APN");
   fona.setGPRSNetworkSettings(F("internet")); //check your network provider
   fona.enableGPS(true);
-  delay(5000); // change to 2000
-  Serial.println(F("Setting GPRS status"));
-  debugPrint("Setting GPRS status");
   fona.enableGPRS(true);
-  delay(5000);  //change to 10000
+  
 }
 
 void loop() {
   float lat, lon, speed_kph, bearing, altitude;
   boolean didGetLock = fona.getGPS(&lat, &lon, &speed_kph, &bearing, &altitude);
-  
-  
-  if (didGetLock)
-  {
-    Serial.println(lat, 5); // the 4 gives us lat to four decimal places
-    Serial.println(lon, 5);  // the 4 gives us lon to four decimal places
+
+  if (!didGetLock) {
+    u8x8.clear();
+    u8x8.println("No Lock");
+    }
+  else {
     uint16_t len;
     uint16_t statuscode;
 
-    u8g2.clear();
-    do {
-      char lati[10];
-      dtostrf(lat, 4, 6, lati); //effectively the same as floatToString() defined above, but keeps it in character array form instead of a string
-      u8g2.drawStr(0,12,"Lat:");
-      u8g2.drawStr(0,25, lati);
-      char longi[10]; //these are done immediately before the drawStr() because of strange memory glitches, and this was the only solution found
-      dtostrf(lon, 4, 6, longi); //a tad messy and unconventional, but gets the job done. Especially since these values are only used for printing on the OLED and not actually sent over 3G
-      u8g2.drawStr(0,38,"Long:"); //For dtostrf() documentation, search for "avr libc dtostrf", but there isn't very much
-      u8g2.drawStr(0,51, longi);
-    } while ( u8g2.nextPage() );
-    delay(1000);
-    //m++;
-    //if ( m == 60 )
-      //m = 0;
-
-// String url = "http://cmaps.knox.nsw.edu.au/location/push?uuid=1&latitude=-99&longitude=7777";
+    //Template url = "http://cmaps.knox.nsw.edu.au/location/push?uuid=1&latitude=-99&longitude=7777";
     String url = F("http://cmaps.knox.nsw.edu.au/location/push?");
     url += F("uuid=");
     url += String(DeviceUUID);
     url += F("&latitude=");
 
+    u8x8.clear();
     char buff[25];
-    floatToString(buff, lat , 8, 5);
+    String printString = "Lat:";
+    floatToString(buff, lat , 5, 5);
     url += buff;
+    printString += buff;
+    u8x8.println(printString);
     url += F("&longitude=");
-    floatToString(buff, lon , 8, 5);
+    printString = "Long:";
+    floatToString(buff, lon , 5, 5);
     url += buff;
+    printString += buff;
+    u8x8.println(printString);
 
-//    boolean httpGetStartRes = fona.HTTP_GET_start(buff, &statuscode, &len);
-    char url_chars[url.length()+1];
-    url.toCharArray(url_chars, url.length()+1);
-    boolean httpGetStartRes = fona.HTTP_GET_start(url_chars, &statuscode, &len); //slightly changed to fix http issue MValent 17/3/2017
+    boolean httpGetStartRes = fona.HTTP_GET_start_From_String(&url, &statuscode, &len);
     fona.HTTP_GET_end();
 
-    if (!httpGetStartRes)
+    if (httpGetStartRes)
     {
-      RestartCount +=1;
-      Serial.println("Restart Count: ");
-      Serial.println( RestartCount);
-      
-      u8g2.clear();
-      do {
-        char lati[10];
-        dtostrf(lat, 4, 6, lati);
-        u8g2.drawStr(0,12,"Lat:");
-        u8g2.drawStr(0,25, lati);
-        char longi[10];
-        dtostrf(lon, 4, 6, longi);
-        u8g2.drawStr(0,38,"Long:");
-        u8g2.drawStr(0,51, longi);
-        u8g2.drawStr(0, 64, "Send To Server Fail"); //Some remnant from previous LCD code? Kept here incase needed
-      } while ( u8g2.nextPage() );
-      
-      
-      if (RestartCount == 4) {
-        RestartCount = 0;
-        Serial.println("HTTP failure response, rebooting in 10s");
-        OLEDPrint("HTTP Failure, rebooting");
-        delay(10000);
-        asm volatile ("  jmp 0");
-      }
-   }
-   else{
-    RestartCount = 0;
+    u8x8.setCursor(0,2);
+    u8x8.println("Sent!");
    }
 
     // 5 minutes delay
-    unsigned long start = millis();
-    Serial.println("delay");    
+    unsigned long start = millis();  
     while (millis() < ((start+300000))) {
       delay(60000);
-     }
-    
-    Serial.println("finished delay");    
-  }
-  else {
-    Serial.println("No lock");
-    OLEDPrint("No Lock");
+    }   
   }
 }
-
